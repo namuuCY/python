2760a6448a05eba30ae24a183f200e5f18602efe
Jang CY <wkdcjfdud13@gmail.com>
2024-01-11 23:08:59 +0900

    연습해봄

---
+++ test1.py
@@ -0,0 +1,20 @@
+a=5
+b=3
+print (a**b)
+
+c=[1,2,3,4,5,6,7,8]     #리스트 자료형이라고 함
+print(c)
+print(c[4])
+
+d=[]
+print(d)
+
+n1=10
+e=[0]*n1
+print(e)
+
+print(c[-3])
+print(c[1:4])           #2~4번째까지 출력.   숫자 민감
+
+arr=[i for i in range(20) if i%2==1]                
+print(arr[-2])
