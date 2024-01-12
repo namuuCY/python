795dc4a36064ae9221504e4596e55e2d8640f051
Jang CY <wkdcjfdud13@gmail.com>
2024-01-12 18:48:18 +0900

    이전꺼 다 올라감?

---
+++ doit!/ch1/p_47.py
@@ -0,0 +1,23 @@
+import random
+
+n=int(input("난수의 개수?"))
+
+for _ in range(n):
+    r= random.randint(10,19)
+    print(r, end=' ')
+    if r==13:
+        print("\n 프로그램을 중단합니다")
+        break
+else:
+    print("\n 난수 생성 종료")
+
+
+
+
+a=int(input())
+
+for i in range(1, a+1):
+    if i*i > a:
+        break
+    if a%i: continue  #0이 아닌값은 다 1임. 코드돌려 확인해봄 이건똑같네
+    print(f"{i}X{a//i}")
