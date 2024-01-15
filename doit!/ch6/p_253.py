b90f0e52139fadc9e22340b281a9e04216dcf9c6
Jang CY <wkdcjfdud13@gmail.com>
2024-01-15 20:49:10 +0900

    일단

--- doit!/ch6/p_253.py
+++ doit!/ch6/p_253.py
@@ -1,1 +1,19 @@
 from typing import MutableSequence
+
+def shell2(a: MutableSequence) -> None:
+
+    n = len(a)
+    h = 1
+
+    while h < n // 9:
+        h = h * 3 + 1
+
+    while h>0:
+        for i in range(h, n):
+            j = i - h
+            tmp = a[i]
+            while j >= 0 and a[j] > tmp:
+                a[j + h] = a[j]
+                j -= h
+            a[j + h] = tmp
+        h //= 3
