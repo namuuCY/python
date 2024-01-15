6db3b0583fd240dbd145ed5e2d360e9f761ab2e5
Jang CY <wkdcjfdud13@gmail.com>
2024-01-15 20:35:25 +0900

    tw

--- doit!/ch6/p_250.py
+++ doit!/ch6/p_250.py
@@ -8,3 +8,24 @@ def shell_sort(a: MutableSequence) -> None:
     while h > 0:
         for i in range(h, n):
             j = i - h
+            tmp = a[i]
+            while j>= 0 and a[j] > tmp:
+                a[j + h] = a[j]
+                j -= h
+            a[j + h] = tmp
+        h //= 2
+
+if __name__ == '__main__':
+    print('셀 정렬 수행 ')
+    num = int(input('원소의 수를 입력하세요. '))
+    x = [None] * num
+
+    for i in range(num):
+        x[i] = int(input(f'x[{i}] = '))
+
+    shell_sort(x)
+
+    print('오름차순 정렬 완료.')
+    for i in range(num):
+        print(f'x[{i}] = {x[i]}')
+
