80e50e1496fcc31fe0e0db72e1f6a2a704671eb4
Jang CY <wkdcjfdud13@gmail.com>
2024-01-16 23:58:38 +0900

    a

--- doit!/ch8/p_344.py
+++ doit!/ch8/p_344.py
@@ -9,4 +9,33 @@ class Node:
 
         self.data = data
         self.next = next
-        self.d
+        self.dnext = dnext
+
+class ArrayLinkedList:
+
+    def __init__(self, capacity: int):
+        
+        self.head = Null
+        self.current = Null
+        self.max = Null
+        self.deleted = Null
+        self.capacity = capacity
+        self.n = [None()] * self.capacity
+        self.no = 0
+
+    def __len__(self) -> int:
+
+        return self.no
+    
+    def get_insert_index(self):
+        if self.deleted == Null:
+            if self.max + 1 < self.capacity:
+                self.max += 1
+                return self.max
+            else:
+                return Null
+        
+        else:
+            rec = self.deleted
+            self.deleted = self.n[rec].dnext
+            return rec
