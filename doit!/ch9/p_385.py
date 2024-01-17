fa2ecd234e6280f9a5595ec4cd71bf8c9586420b
Jang CY <wkdcjfdud13@gmail.com>
2024-01-17 13:47:44 +0900

    ㅁ

--- doit!/ch9/p_385.py
+++ doit!/ch9/p_385.py
@@ -16,3 +16,29 @@ class BinarySearchTree:
     def __init__(self):
 
         self.root = None
+
+
+    def search(self, key: Any) -> Any:
+
+        p = self.root
+        while True:
+            if p is None:
+                return None
+            if key == p.key:
+                return p.value
+            elif key < p.key:
+                p = p.left
+            else:
+                p = p.right
+
+    def add(self, key: Any, value: Any) -> bool:
+
+        def add_node(node: Node, key: Any, value: Any) -> None:
+
+            if key == node.key:
+                return False
+            elif key < node.key:
+                if node.left is None:
+                    node.left = Node(key, value, None, None)
+                else:
+                    add_node(node.left, key, value)
