90fe46265aa2c2b340bdc1531d604d18ef97bdb7
Jang CY <wkdcjfdud13@gmail.com>
2024-01-17 14:50:34 +0900

    a

--- doit!/ch9/p_385.py
+++ doit!/ch9/p_385.py
@@ -42,3 +42,73 @@ class BinarySearchTree:
                     node.left = Node(key, value, None, None)
                 else:
                     add_node(node.left, key, value)
+            else:
+                if node.right is None:
+                    node.right = Node(key, value, None, None)
+                else:
+                    add_node(node.right, key, value)
+                
+            return True
+        
+        if self.root is None:
+            self.root = Node(key, value, None, None)
+            return True
+        else:
+            return add_node(self.root, key, value)
+        
+    
+    def remove(self, key: Any) -> bool:
+
+        p = self.root
+        parent = None
+        is_left_child = True
+
+        while True:
+            if p is None:
+                return False
+            
+            if key == p.key:
+                break
+            else:
+                parent = p
+                if key < p.key:
+                    is_left_child = True
+                    p = p.left
+                else:
+                    is_left_child = False
+                    p = p.right
+        
+        if p.left is None:
+            if p is self.root:
+                self.root = p.right
+            elif is_left_child:
+                parent.left = p.right
+            else:
+                parent.right = p.right
+        elif p.right is None:
+            if p is self.root:
+                self.root = p.left
+            elif is_left_child:
+                parent.left = p.left
+            else:
+                parent.right = p.left
+
+        else:
+            parent = p
+            left = p.left
+            is_left_child = True
+            while left.right is not None:
+                parent = left
+                left = left.right
+                is_left_child = False
+
+            p.key = left.key
+            p.value = left.value
+            if is_left_child:
+                parent.left = left.left
+            else:
+                parent.right = left.left
+
+        return True
+
+            
