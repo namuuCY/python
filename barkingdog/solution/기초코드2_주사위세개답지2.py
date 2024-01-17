65484b99fb3047ccd656f203a038259076dc132a
Jang CY <wkdcjfdud13@gmail.com>
2024-01-18 00:11:22 +0900

    a

--- barkingdog/solution/기초코드2_주사위세개답지2.py
+++ barkingdog/solution/기초코드2_주사위세개답지2.py
@@ -8,7 +8,7 @@ x.sort()
 if x[0] == x[2]:
     print(x[0] * 1000 + 10000)
 elif x[0] == x[1] or x[0] == x[2]:
-    print(x[0 * 100 + 1000])
+    print(x[0] * 100 + 1000)
 elif x[1] == x[2]:
     print(x[1] * 100 + 1000)
 else:
