7ca8aacd9bbc6d29614203c296a9751072c1d460
Jang CY <wkdcjfdud13@gmail.com>
2024-01-17 23:59:37 +0900

    a

--- barkingdog/solution/기초코드2_주사위세개.py
+++ barkingdog/solution/기초코드2_주사위세개.py
@@ -3,3 +3,17 @@ import sys
 dice = sys.stdin.readline
 list = list(map(int, dice().split()))
 
+if list[0] == list[1] == list[2]:
+    print(list[0] * 1000 + 10000)
+
+elif list[0] == list[1] and list[0] != list[2]:
+    print(list[0] * 100 + 1000)
+
+elif list[0] == list[2] and list[1] != list[2]:
+    print(list[0] * 100 + 1000)
+
+elif list[1] == list[2] and list[0] != list[2]:
+    print(list[1] * 100 + 1000)
+
+else:
+    print(max(list) * 100)
