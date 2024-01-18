df382a7c46ed2af82b5dac62c5a9f19ab4c0f95c
Jang CY <wkdcjfdud13@gmail.com>
2024-01-18 23:20:49 +0900

    a

--- barkingdog/solution/연결리스트/연결리스트_에디터.py
+++ barkingdog/solution/연결리스트/연결리스트_에디터.py
@@ -11,22 +11,21 @@ n = int(sys.stdin.readline().rstrip())
 for i in range(n):
 
     flist = list(map(str, sys.stdin.readline().split()))
-    
+
     if flist[0] == 'L':
-        if idx == 0 : continue
-        else: idx -= 1
+        if idx > 0:
+            idx -= 1
 
-    if flist[0] == 'D':
-        if idx == len(ans): continue
-        else: idx += 1
+    elif flist[0] == 'D':
+        if idx < len(ans):
+            idx += 1
 
-    if flist[0] == 'B':
-        if idx == 0: continue
-        else: 
+    elif flist[0] == 'B':
+        if idx > 0:
             ans.remove(ans[idx - 1])
             idx -= 1
 
-    if flist[0] == 'P':
+    elif flist[0] == 'P':
         if idx == len(ans):
             ans.append(flist[1])
             idx += 1
