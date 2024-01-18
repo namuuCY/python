cabcd272723fddb41e08196572c94088cdb4da44
Jang CY <wkdcjfdud13@gmail.com>
2024-01-18 11:30:56 +0900

    오답..

--- barkingdog/solution/기초코드2_카드역배치.py
+++ barkingdog/solution/기초코드2_카드역배치.py
@@ -1,24 +1,18 @@
 import sys
 from typing import MutableSequence
 
-def reverse(x: MutableSequence, a: int, b: int) -> list:
+def reverse(x: MutableSequence, a: int, b: int) -> None:
     a -= 1
     b -= 1
-    for i in range(a, (a + b) // 2):
+    for i in range(a, (a + b) // 2 + 1):
         x[i], x[a + b - i] = x[a + b - i], x[i]
     
  
-        
-
 read = sys.stdin.readline
 x = list(range(1, 21))
 matrix = [list(map(int, read().split())) for _ in range(10)]
-
-
 
 for i in range(10):
     reverse(x, matrix[i][0], matrix[i][1])
 
 print(*x, sep = ' ')
-
-    
