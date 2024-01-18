9c86952b285d54eff037c6265c5e32a1a1b302e9
Jang CY <wkdcjfdud13@gmail.com>
2024-01-19 01:27:47 +0900

    ㅁ

--- barkingdog/오답노트/큐_큐.py
+++ barkingdog/오답노트/큐_큐.py
@@ -4,8 +4,8 @@ import sys
 n = int(sys.stdin.readline().rstrip())
 data = deque()
 for _ in range(n):
-    order = sys.stdin.readline().split()
-    if order[0] == 'push':
+    order = sys.stdin.readline().split()        # sys.stdin.readline().split()은
+    if order[0] == 'push':                      # 이미 list다! split()은 공백으로 구분한다는뜻
         data.append(int(order[1]))
     elif order[0] == 'pop':         # 더 줄일수있음
         if not data:                # elif order[0] == 'pop':
