4d6d2cded7580f21054f042e7415d72bb2974d51
Jang CY <wkdcjfdud13@gmail.com>
2024-01-20 02:10:19 +0900

    a

--- barkingdog/solution/스택/오아시스재결합.py
+++ barkingdog/solution/스택/오아시스재결합.py
@@ -5,9 +5,28 @@ stack = deque()
 n = int(input())
 count = 0
 for _ in range(n):
-    
     k = int(sys.stdin.readline().rstrip())
-    while 
+    if stack:
+        while stack and stack[-1] <= k:
+            if stack[-1] == k:
+                count += 1
+                break
+            else: 
+                stack.pop()
+                count += 1
+            
+            
+        if stack:
+            stack.append(k)
+            count += 1
+            
+        else:
+            stack.append(k)
+            
+
+    else:
+        stack.append(k)
+        
 
 print(count)
         
