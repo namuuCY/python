ee5dd73b0c2154c0e57d55db5cfb051add5fb6a6
Jang CY <wkdcjfdud13@gmail.com>
2024-01-19 21:14:48 +0900

    a

---
+++ barkingdog/solution/스택/옥상정원꾸미기.py
@@ -0,0 +1,26 @@
+from collections import deque
+import sys
+
+n = int(input().rstrip())
+towers = []
+stk = deque()
+stk.append((0, 1000000001))
+count = []
+for _ in range(n):
+    towers.append(int(sys.stdin.readline().rstrip()))
+
+towers.reverse()
+
+for i in range(n):
+    while stk[-1][1] >= towers[i]:
+        count.append(i - stk[-1][0])
+        stk.append((i + 1, towers[i]))
+        break
+    else:
+        stk.pop()
+
+print(count)        
+print(sum(count))
+
+
+
