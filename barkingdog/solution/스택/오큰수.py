0d79e7f9d38af3967c26d37f5f875394c1804d8d
Jang CY <wkdcjfdud13@gmail.com>
2024-01-19 22:11:28 +0900

    a

--- barkingdog/solution/스택/오큰수.py
+++ barkingdog/solution/스택/오큰수.py
@@ -2,4 +2,19 @@ from collections import deque
 import sys
 
 n = int(sys.stdin.readline().rstrip())
-nums = map(int, sys.stdin.readline().split())
+num = list(map(int, sys.stdin.readline().split()))
+stk = deque()
+stk.append(2000000)
+NGE = []
+for i in range(n-1, -1, -1):
+    while stk[-1] <= num[i]:
+        stk.pop()
+    else:
+        if stk[-1] > 1000000:
+            NGE.append(-1)
+        else:
+            NGE.append(stk[-1])
+        stk.append(num[i])
+    
+NGE.reverse()
+print(*NGE, sep = ' ')
