9d599e314351b5b7dda8426abaeac84a0dab51bf
Jang CY <wkdcjfdud13@gmail.com>
2024-01-21 01:41:27 +0900

    a

--- barkingdog/solution/수학/링.py
+++ barkingdog/solution/수학/링.py
@@ -4,4 +4,6 @@ n = int(input())
 data = [] * n
 data = list(map(int, sys.stdin.readline().split()))
 
-print(data)
+for i in range(1, n):
+    cd = math.gcd(data[0], data[i])
+    print(f"{data[0] // cd}/{data[i] // cd}")
