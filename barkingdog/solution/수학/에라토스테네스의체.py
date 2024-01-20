69a49780b20c98e8c6a488b998c8767780f51608
Jang CY <wkdcjfdud13@gmail.com>
2024-01-20 22:45:49 +0900

    a

---
+++ barkingdog/solution/수학/에라토스테네스의체.py
@@ -0,0 +1,8 @@
+import sys
+
+n, k = list(map(int, sys.stdin.readline().split()))
+ans = [0] * 1001
+
+
+for _ in range()
+
--- barkingdog/solution/수학/캠핑.py
+++ barkingdog/solution/수학/캠핑.py
@@ -1,11 +1,16 @@
 import sys
 
+i = 0
 while True:
     data = str(sys.stdin.readline().rstrip())
     if data == '0 0 0':
         break
     L, P, V = list(map(int, data.split()))
-    
+    i += 1
+    ans = (V // P) * L + min(V % P, L)
+    print(f"Case {i}: {ans}")
+
+
 
 
 
