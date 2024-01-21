a85626e68e8f792146f15d0f7ba893e7fdbbee86
Jang CY <wkdcjfdud13@gmail.com>
2024-01-21 21:33:15 +0900

    a

--- barkingdog/solution/무한수열(2차).py
+++ barkingdog/solution/무한수열(2차).py
@@ -1,2 +1,22 @@
 from collections import deque
+import sys
+n, p, q = map(int, sys.stdin.readline().split())
+
+list = deque()
+list.append(n)
+ans = 0
+while list:
+    tmp = list.pop()
+    if tmp == 0:
+        ans += 1
+    else:
+        tmpa = tmp // p
+        tmpb = tmp // q
+        list.append(max(tmpa, tmpb))
+        list.append(min(tmpa, tmpb))
+
+print(ans)
+    
+
+    
 
