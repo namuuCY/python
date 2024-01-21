122e044118f91c7f8f41f33a1f5d4b8c2ac47e2e
Jang CY <wkdcjfdud13@gmail.com>
2024-01-21 22:44:11 +0900

    a

---
+++ barkingdog/오답노트/그림.py
@@ -0,0 +1,41 @@
+from collections import deque
+import sys
+
+n, m = map(int, sys.stdin.readline().split())   #map(int, input().split())이 보기좋다
+vis = [[False] * m for _ in range(n)]
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+queue = deque()
+paint = []
+board = []
+#############################################################################
+for _ in range(n):
+    board.append(list(map(int, sys.stdin.readline().split())))
+#                                                                       
+# board = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]  #
+#############################################################################
+for i in range(n):
+    for j in range(m):
+        if board[i][j] == 1 and not vis[i][j]:
+            count = 1
+            queue.append((i, j))
+            vis[i][j] = True
+            while queue:
+                x , y  = queue.popleft()
+                for dir in range(4):
+                    nx, ny = x + dx[dir], y + dy[dir]
+                    if nx < 0 or nx >= n or ny < 0 or ny >= m:
+                        continue
+                    if vis[nx][ny] or board[nx][ny] != 1:
+                        continue
+                    vis[nx][ny] = True
+                    queue.append((nx, ny))
+                    count += 1
+            paint.append(count)
+
+if paint:
+    print(len(paint))
+    print(max(paint))
+else:
+    print(0)
+    print(0)
