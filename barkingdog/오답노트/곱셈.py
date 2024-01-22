8b536f371a83cbc32a3a6f437fbe4041b3264b26
Jang CY <wkdcjfdud13@gmail.com>
2024-01-22 23:01:30 +0900

    a

--- barkingdog/오답노트/곱셈.py
+++ barkingdog/오답노트/곱셈.py
@@ -1,3 +1,20 @@
+def func(a: int, b: int, c: int) -> int:
+    if b == 1:
+        return a % c
+    elif b % 2 == 0:
+        val = func(a, b//2, c)
+        return val * val % c
+    else:
+        val = func(a, b//2, c)
+        return val * val * a % c
+    
+a, b, c = list(map(int, input().split()))
+print(func(a, b, c))
+
+
+# 밑의 함수는 재귀함수 안에 재귀함수를 두번 불러서 시간복잡도가 너무 길어지는 결과 초래함
+# 재귀함수 안에 재귀함수가 두번이상 들어갈경우 시간복잡도가 logn 이 아니라 n 이 되는 결과 초래함
+"""
 def func(p: int, q: int, r: int) -> int:
     if q == 0:                   # 무식하지만 속도는 빨랐죠? 존나빨랐죠?
         return 1
@@ -25,3 +42,4 @@ def func(p: int, q: int, r: int) -> int:
 a, b, c = list(map(int, input().split()))
 print(func(a, b, c))
 
+"""
