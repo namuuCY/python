eb721872cccb33052c5dd1355f328eea7a16be59
Jang CY <wkdcjfdud13@gmail.com>
2024-01-22 15:15:07 +0900

    a

--- barkingdog/solution/BFS/유기농배추.py
+++ barkingdog/solution/BFS/유기농배추.py
@@ -1,3 +1,31 @@
 from collections import deque
-import sys
+
+trial = int(input().rstrip())
+dx = [1, 0, -1, 0]
+dy = [0, 1, 0, -1]
+
+for _ in range(trial):
+    n, m, k = map(int, input().split())
+    board = [[0] * m for _ in range(n)]
+    vis = [[False] * m for _ in range(n)]
+    count = 0
+    Q = deque()
+    for _ in range(k):
+        i, j = map(int, input().split())
+        board[i][j] = 1
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == 1 and not vis[i][j]:
+                Q.append((i, j))
+                vis[i][j] = True
+                while Q:
+                    x, y = Q.popleft()
+                    for i in range(4):
+                        nx, ny = x + dx[i], y + dy[i]
+                        if 0 <= nx < n and 0 <= ny < m and not vis[nx][ny]:
+                            vis[nx][ny] = True
+                            Q.append((nx,ny))
+                count += 1
+    print(count)
+        
 
