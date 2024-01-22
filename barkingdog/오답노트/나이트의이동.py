012dbcbb3a8e5958056900693e335dba2cb8a06f
Jang CY <wkdcjfdud13@gmail.com>
2024-01-22 21:58:31 +0900

    a

--- barkingdog/오답노트/나이트의이동.py
+++ barkingdog/오답노트/나이트의이동.py
@@ -7,27 +7,45 @@ Q = deque()
 
 for _ in range(trial):
     l = int(input().rstrip())
+    dist = [[-1]* l for _ in range(l)]
+    pos = list(map(int, input().split()))
+    des = list(map(int, input().split()))
+    Q.append((pos[0], pos[1]))
+    dist[pos[0]][pos[1]] = 0
+    while Q:
+        x, y = Q.popleft()
+        for i in range(8):
+            nx, ny = x + dx[i], y + dy[i]
+            if nx < 0 or nx >= l or ny < 0 or ny >= l:
+                continue
+            if dist[nx][ny] >= 0:
+                continue
+            dist[nx][ny] = dist[x][y] + 1
+            Q.append((nx, ny))
+    print(dist[des[0]][des[1]])
+
+'''
+for _ in range(trial):
+    l = int(input().rstrip())
     dist = [[-1] * l for _ in range(l)]
     pos = list(map(int, input().split()))     # map = list 아님... 
     des = list(map(int, input().split()))
-    if pos == des:
-        print(0)
-        continue     # pos = des 일 경우도 고려해야함.
+    # pos = des 일 경우도 고려해야함.
     Q.append(pos)
     dist[pos[0]][pos[1]] = 0
     while Q:
         x, y = Q.popleft()
         for i in range(8):
             nx, ny = x + dx[i], y + dy[i]
-            if nx == des[0] and ny == des[1]:
+            if nx == des[0] and ny == des[1]:       # 여기서 미리 뭐를 하려고 하니까 예외처리가 안됨
                 print(dist[x][y] + 1)
                 while Q:
                     Q.pop()          # 이거 pop할게 남아서 계속 돌리고있네
                 break                        # 이거 3번 돌려야하는데 exit 하면안되지...
-            if 0 <= nx < l and 0 <= ny < l and dist[nx][ny] == -1:
+            if 0 <= nx < l and 0 <= ny < l and dist[nx][ny] < 0 :
                 dist[nx][ny] = dist[x][y] + 1
                 Q.append((nx, ny))     
-
+'''
 
 
     
