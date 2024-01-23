9a4bc8b21f6eadaa5c91c79fcf0d7db53c3da9fa
Jang CY <wkdcjfdud13@gmail.com>
2024-01-23 23:50:33 +0900

    a

--- barkingdog/solution/BFS/안전영역.py
+++ barkingdog/solution/BFS/안전영역.py
@@ -1,3 +1,10 @@
 from collections import deque
 import sys
 
+dir = [[1,0], [0, 1], [-1, 0], [0, -1]]
+Q = deque()
+
+
+
+n = int(input().rstrip())
+board = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
