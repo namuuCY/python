b213abf85b8c2f1480915c32ecb92b82a69cfeaa
Jang CY <wkdcjfdud13@gmail.com>
2024-01-25 13:56:39 +0900

    a

--- barkingdog/solution/BFS/빙산.py
+++ barkingdog/solution/BFS/빙산.py
@@ -7,8 +7,10 @@ n, m = map(int, input().split())
 ice = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
-
+vis = [[[0] * 100 for _ in range(m)] for _ in range(n)]
+Q = deque()
 def melt() -> tuple:
+    searched = False
     for i in range(n):
         for j in range(m):
             if ice[i][j] == 0:
@@ -16,6 +18,39 @@ def melt() -> tuple:
                     ni, nj = i+dx[dir], j+dy[dir]
                     if ice[ni][nj] != 0:
                         ice[ni][nj] -= 1
+            if ice[i][j] != 0 and not searched:
+                for dir in range(4):
+                    ni, nj = i+dx[dir], j+dy[dir]
+                    if ice[ni][nj] == 0:
+                        break
+                else:
+                    searched = True
+                    point = (i, j)
+    if searched:
+        return point    #점을 순서쌍으로 출력
+    else:
+        
+
+def bfs(tup: tuple, viscount: int) -> None:   # 순서쌍 점 기준으로 bfs를 돌려봄
+    Q.append(tup)
+    tmpx, tmpy = tup
+    vis[viscount][tmpx][tmpy] = 1
+    while Q:
+        x, y = Q.popleft()
+        for d in range(4):
+            nx, ny = x + dx[d], y + dy[d]
+            if 0<= nx < n and 0 <= ny < m and ice[nx][ny] != 0 and vis[viscount][nx][ny] == 0:
+                vis[viscount][nx][ny] = 1
+                Q.append((nx, ny))
+    for i in range(n):
+        for j in range(m):
+            if ice[i][j] != 0 and vis[viscount][i][j] == 0 :
+                return viscount
+    return 0            
+
+for year in range(1, 100):
+
+
 
 
     
