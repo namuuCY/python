d8e64ed58a6492abda4b732e79f97ca653781034
Jang CY <wkdcjfdud13@gmail.com>
2024-01-26 12:22:48 +0900

    a

--- barkingdog/solution/백트래킹/N퀸문제.py
+++ barkingdog/solution/백트래킹/N퀸문제.py
@@ -1,2 +1,31 @@
-n = int(input)
+n = int(input())
 
+
+isusedR = [False] * n
+isusedD = [False] * (2 * n -1)
+isusedU = [False] * (2 * n -1)
+
+count = 0
+
+def placed(x: int, y: int) -> bool:
+    return isusedR[y] and isusedD[x + y] and isusedU[n - 1 - x + y]
+
+def nqueen(k: int):
+    global count
+    if k == n:
+        count += 1
+        return
+    for i in range(n):
+        if not placed(k, i):
+            isusedR[i] = True
+            isusedD[k+i] = True
+            isusedU[n-1-k+i] = True
+            nqueen(k + 1)
+            isusedR[i] = False
+            isusedD[k+i] = False
+            isusedU[n-1-k+i] = False
+
+
+nqueen(0)
+
+print(count)
