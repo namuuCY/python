39de505cec7990c0b5063b323a2827ded764af33
Jang CY <wkdcjfdud13@gmail.com>
2024-01-26 14:04:45 +0900

    ㅁ

--- barkingdog/solution/백트래킹/부분수열의합.py
+++ barkingdog/solution/백트래킹/부분수열의합.py
@@ -3,4 +3,24 @@ import sys
 n, s = map(int, sys.stdin.readline().split())
 seq = list(map(int, sys.stdin.readline().split()))
 
+isused = [0] * n
+count = 0
+ansarr = []
+def summation(k: int):
+    global count
+    if k == n :
+        if ansarr and sum(ansarr) == s:
+            count += 1
+        return
+    for i in range(2):
+        if i == 1:
+            isused[k] = 1
+            ansarr.append(seq[k])
+            summation(k + 1)
+            ansarr.pop()
+            isused[k] = 0
+        else:
+            summation(k + 1)
+summation(0)
 
+print(count)
