81352bb855f0cf2e5a308675c020a87dbd9f5854
Jang CY <wkdcjfdud13@gmail.com>
2024-01-26 14:43:00 +0900

    a

--- barkingdog/solution/백트래킹/N과M3.py
+++ barkingdog/solution/백트래킹/N과M3.py
@@ -1,2 +1,12 @@
 import itertools
 
+n, m = map(int, input().split())
+
+seqs = itertools.product(range(1, n + 1), repeat = m)
+    
+    # 이거 product가 집합사이의 데카르트곱임
+    # 따라서 변수는 iterable이 두개거나
+    # repeat = 2와 같이 되어야함.
+
+for i in seqs:
+    print(*i, sep = ' ')
