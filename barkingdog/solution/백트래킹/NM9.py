e57ce09164981e7bb9cacbce1031bc6e4c57f0cc
Jang CY <wkdcjfdud13@gmail.com>
2024-01-26 16:53:14 +0900

    a

--- barkingdog/solution/백트래킹/NM9.py
+++ barkingdog/solution/백트래킹/NM9.py
@@ -1,2 +1,10 @@
 import itertools
 
+n, m = map(int, input().split())
+numbers = list(map(int, input().split()))
+numbers.sort()
+
+comb = itertools.permutations(numbers, m)
+
+for i in comb:
+    print(*i, sep = ' ')
