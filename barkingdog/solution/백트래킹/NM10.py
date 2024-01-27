ad7a940ec0051c92c01d6faed9888c755ae04eb0
Jang CY <wkdcjfdud13@gmail.com>
2024-01-27 17:01:39 +0900

    a

--- barkingdog/solution/백트래킹/NM10.py
+++ barkingdog/solution/백트래킹/NM10.py
@@ -1,1 +1,22 @@
 n, m = map(int, input().split())
+numbers = list(map(int, input().split()))
+numbers.sort()
+isused = [0] * n
+arr = []
+def func(k):
+    tmp = -1  # 마지막 원소 저장
+    if k == m:
+        print(*arr, sep = ' ')
+        return
+    for i in range(n):
+        if isused[i] == 0 and tmp <= i:
+            for j in range(i):
+                isused[j] = 1
+            tmp = i
+            arr.append(numbers[i])
+            func(k + 1)
+            arr.pop()
+            for j in range(i):
+                isused[j] = 0
+        
+func(0)
