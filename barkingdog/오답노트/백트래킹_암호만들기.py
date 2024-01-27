a00ce6774e83f834618358441001cd6b45eddba3
Jang CY <wkdcjfdud13@gmail.com>
2024-01-28 00:37:00 +0900

    ㅁ

---
+++ barkingdog/오답노트/백트래킹_암호만들기.py
@@ -0,0 +1,31 @@
+
+n, m = map(int, input().split())
+data = list(map(str, input().split()))
+data.sort()
+
+acount = 0
+adata = ['a','e','i','o','u']
+bcount = 0
+arr = []
+
+def func(k, st):
+    global acount
+    global bcount
+    if k == n:
+        if acount >= 1 and bcount >= 2:
+            print(''.join(arr))
+            return
+    for i in range(st, len(data)):      # range 안써서 인덱스 오류남..
+        if data[i] in adata:
+            acount += 1
+        else:
+            bcount += 1
+        arr.append(data[i])             # 이거 원소 추가하는것도 안햇음
+        func(k + 1, i + 1)
+        arr.pop()
+        if data[i] in adata:
+            acount -= 1                 # 빼기를 안했음..
+        else:
+            bcount -= 1
+
+func(0, 0)
