56cf83567f76a8f20dafb853787239ab15b7d4b8
Jang CY <wkdcjfdud13@gmail.com>
2024-01-30 01:48:43 +0900

    a

---
+++ barkingdog/solution/시뮬레이션/스티커붙이기.py
@@ -0,0 +1,16 @@
+import sys
+n, m, k = map(int, input().split())
+sticker = []
+tmp = []
+for i in range(k):
+    x, y = map(int, input().split())
+    sticker[i] = [list(map(int, sys.stdin.readline().split())) for _ in range(x)]
+
+def rot(i ):      # 몇번째 스티커를 얼마만큼 돌릴거냐?
+
+        
+def add():
+
+
+def bfs():
+
