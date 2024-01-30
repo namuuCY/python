5a152c8509e085d07c1a314e97c511ecf1e8313c
Jang CY <wkdcjfdud13@gmail.com>
2024-01-31 01:22:59 +0900

    a

--- barkingdog/solution/시뮬레이션/2048.py
+++ barkingdog/solution/시뮬레이션/2048.py
@@ -4,17 +4,13 @@ import sys
 
 n = int(input().rstrip())
 board = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+ans = 0
 # 4진법 써먹어보자.
 
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
 
-def lift(dir):
-    if dir == 0:    
-        for i in range(n):
-            for j in range(n):
-                if i == 0:
-                
+       
 
 for trial in range(4**5):
     board2 = deepcopy(board)
+
+    ans = max(ans, max(board2[i][j] for i in range(n) for j in range(n)))
---
+++ barkingdog/solution/시뮬레이션/test.py
@@ -0,0 +1,4 @@
+board = [[1,2,3,4], [5,6,7,8]]
+board[1] = board[1][::-1]
+
+print(board)
