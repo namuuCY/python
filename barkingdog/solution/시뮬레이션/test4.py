517614d01578ae189ff7a6632e59eb06bdba9d85
Jang CY <wkdcjfdud13@gmail.com>
2024-02-03 22:51:57 +0900

    ㅁ

--- barkingdog/solution/시뮬레이션/test4.py
+++ barkingdog/solution/시뮬레이션/test4.py
@@ -1,3 +1,8 @@
-from math import factorial
+import sys
+from itertools import combinations
 
-print(factorial(20)//(factorial(10)**2))
+N = int(sys.stdin.readline())
+stat = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+sum_stat = [sum(i) + sum(j) for i, j in zip(stat, zip(*stat))]
+
+print(*sum_stat, sep = '\n')
