9e41192d7baed9d25cc5842ed1f2d86e22f1056b
Jang CY <wkdcjfdud13@gmail.com>
2024-02-05 17:05:15 +0900

    ㅁ

--- barkingdog/오답노트/(어려움+가지치기)시뮬_사다리조작.py
+++ barkingdog/오답노트/(어려움+가지치기)시뮬_사다리조작.py
@@ -5,15 +5,15 @@ import sys
 # N이 세로선 개수 H가 점선개수
 N, M, H = map(int, input().split())
 board = [[0] * (N + 1) for _ in range(H + 1)]
-ladder_list = [0] * (N + 1)
+
 for _ in range(M):
     t1, t2 = map(int, input().split())
     board[t1][t2] = 1
-    ladder_list[t2] += 1
+           # 사다리에 홀수인게 있으면 그만큼 더해줘야 가능성이 있어서 이것도 가지치기
 
 
 def ck():
-    for i in range(1, N):       # N-1번째까지만 하면 마지막건 자동이라
+    for i in range(1, N):       # N-1번째까지만 하면 마지막건 자동이라 가지치기
         now = i
         for j in range(1, H + 1):
             if board[j][now] == 1:
@@ -26,23 +26,19 @@ def ck():
 
 def recur(n, x, y):
     global ans
-    if n >= ans:
+    if n >= ans:                            # 이거 해주면 빠름
         return
     if ck():
         ans = min(ans, n)
         return 
-    if sum([1 for i in ladder_list if i % 2 == 1]) > 3 - n:
-        return
-    for i in range(x, H + 1):
+     # 사다리에 홀수인게 있으면 그만큼 더해줘야 가능성이 있어서 이것도 가지치기
+    for i in range(x, H + 1):                   # 효율적인 가지치기 2 : 다음 사다리의 범위 제한시키기
         for j in range(y if x==i else 1, N):
             if board[i][j - 1] == 1 or board[i][j] == 1 or board[i][j + 1] == 1:
                 continue
             board[i][j] = 1
-            ladder_list[j] += 1
-            recur(n + 1, i, j + 2)
+            recur(n + 1, i, j + 2)              # 효율적인 가지치기 3 : 다음 사다리의 범위 제한시키기
             board[i][j] = 0
-            ladder_list[j] -= 1
-
 
 ans = 4
 recur(0, 1, 1)
