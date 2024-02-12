8614fe8ccb4825e9b344832dc67edaf2797a2fab
Jang CY <wkdcjfdud13@gmail.com>
2024-02-12 17:19:47 +0900

    a

--- barkingdog/solution/다이나믹프로그래밍/동전1.py
+++ barkingdog/solution/다이나믹프로그래밍/동전1.py
@@ -2,3 +2,10 @@ import sys
 
 n, k = map(int, input().split())
 coins = list(map(int, sys.stdin.readlines().rstrip()))
+coins.sort()
+DP = {}
+for i in range(n):
+    DP[(0, coins[i])] = 0
+for i in range(1, k+1):
+    for j in range(n):
+        DP
