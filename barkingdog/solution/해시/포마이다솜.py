1f9738daf3babd8abbb9e17a388f8114d8d04c69
Jang CY <wkdcjfdud13@gmail.com>
2024-06-09 01:19:33 +0900

    ㅁ

--- barkingdog/solution/해시/포마이다솜.py
+++ barkingdog/solution/해시/포마이다솜.py
@@ -1,2 +1,17 @@
-import io, os
+import sys
 
+N, M = map(int, input().split())
+
+pokedicts = {}
+
+for i in range(1, N + 1):
+    name = sys.stdin.readline().strip()     # list는 hash불가능, 무조건 str화 할것!
+    pokedicts[name] = str(i)                # strip()은 str로, split()은 list로 return함
+    pokedicts[str(i)] = name
+
+ans = []
+
+for _ in range(M):
+    ans.append(pokedicts[sys.stdin.readline().strip()])
+
+sys.stdout.write('\n'.join(ans))
