aa8ec959e154948bfdd889d68db2a97ba7cf74a4
Jang CY <wkdcjfdud13@gmail.com>
2024-06-08 19:02:42 +0900

    a

--- barkingdog/solution/이분탐색/test1.py
+++ barkingdog/solution/이분탐색/test1.py
@@ -1,1 +1,17 @@
-s = {-2, 3,4,5,1}
+import sys
+
+input()
+
+numlist = list(map(int, sys.stdin.readline().split()))
+sortednums = list(set(numlist))
+sortednums.sort()
+cardlist = {}
+
+for i in range(len(sortednums)):
+    cardlist[sortednums[i]] = i
+
+answer = []
+
+for num in numlist:
+    answer.append(cardlist.get(num))
+print(*answer, sep=' ')
