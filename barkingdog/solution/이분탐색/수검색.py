a3168b9d80505ac977361ac742d8f941afb48153
Jang CY <wkdcjfdud13@gmail.com>
2024-06-08 17:52:25 +0900

    asdf

---
+++ barkingdog/solution/이분탐색/수검색.py
@@ -0,0 +1,35 @@
+import sys
+
+def BinSearch(target):
+    st = 0
+    ed = N - 1
+    while st <= ed:
+        mid = (st + ed) // 2
+        if NumberList[mid] == target:
+            Answer.append(1)
+            return
+        elif NumberList[mid] < target:
+            st = mid + 1
+        else:
+            ed = mid - 1
+    Answer.append(0)
+    return
+
+N = int(input().rstrip())
+NumberList = list(map(int, sys.stdin.readline().split()))
+NumberList.sort()
+
+M = int(input().rstrip())
+TargetList = list(map(int, sys.stdin.readline().split()))
+
+Answer = []
+
+for target in TargetList:
+    if NumberList[0] == target or NumberList[N - 1] == target:
+        Answer.append(1)
+        continue
+    BinSearch(target)
+
+print(*Answer, sep='\n')
+    
+
