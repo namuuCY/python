e5591052f602be6f1f29e9cd506874d1fca674d7
Jang CY <wkdcjfdud13@gmail.com>
2024-06-08 20:36:08 +0900

    ㅁ

--- barkingdog/solution/이분탐색/랜선자르기.py
+++ barkingdog/solution/이분탐색/랜선자르기.py
@@ -15,6 +15,10 @@ en = 2**31 - 1
 
 while st < en:
     mid = (st + en + 1) // 2
+
+    # st, ed값이 1차이 날때를 생각해보고, mid값이 어디에 되어야하는지를 생각해서 +1인지 아닌지 결정.
+
+
     if overN(mid):
         st = mid
     else: 
