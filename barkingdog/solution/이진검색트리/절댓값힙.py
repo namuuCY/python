3e4a5841e1bf12ad696c83766597f4b2a3ed6706
Jang CY <wkdcjfdud13@gmail.com>
2024-06-10 02:00:32 +0900

    a

--- barkingdog/solution/이진검색트리/절댓값힙.py
+++ barkingdog/solution/이진검색트리/절댓값힙.py
@@ -1,3 +1,18 @@
 import sys, heapq
 
-heapq.heappush()
+
+data = sys.stdin.read().split()
+heap = []
+ans = []
+
+for i in range(1, int(data[0]) + 1):
+    num = int(data[i])
+    if num != 0:
+        heapq.heappush(heap, (abs(num), num))
+    else:
+        if not heap:
+            ans.append(0)
+        else:
+            ans.append(heapq.heappop(heap)[1])
+
+print(*ans, sep = '\n')
