88ca869f254f87bb3a2627d44089c2ea996c7cb3
Jang CY <wkdcjfdud13@gmail.com>
2024-06-10 12:59:11 +0900

    ㅁ

---
+++ barkingdog/solution/그래프/DFSwith재귀.py
@@ -0,0 +1,17 @@
+# DFS와 재귀는 밀접하다.
+
+# 스택메모리 제한이 작을 경우에는 재귀깊이가 클경우 메모리 초과될경우도 있음.
+
+# 참고로 스택을 사용하면 FILO 이고 재귀는 들어오는것마다 하기 때문에 방문 순서가 달라진다.
+
+adj = [[1,2],[2],[3],[0,1,2]]
+vis = [False] * 4
+
+def DFS(cur):
+    vis[cur] = True
+    print(f'{cur}번 노드 방문')
+    for nxt in adj[cur]:
+        if vis[nxt]: continue
+        DFS(nxt)
+
+DFS(0)
