01859ca6fc4c6a7f2e968bd08b30cbc64341bba1
Jang CY <wkdcjfdud13@gmail.com>
2024-06-11 01:07:31 +0900

    a

--- barkingdog/solution/그래프+트리+위상정렬/트리의부모찾기.py
+++ barkingdog/solution/그래프+트리+위상정렬/트리의부모찾기.py
@@ -1,4 +1,30 @@
 import sys
+from collections import deque
+
+
+def DFS(root):
+    Q.append(root)
+    P[root] = 0
+    while Q:
+        cur = Q.pop()
+        for nxt in adj[cur]:
+            if P[nxt] != -1: continue
+            Q.append(nxt)
+            P[nxt] = cur
+
 
 N = int(input())
 adj = [[] for _ in range(N)]
+Q = deque()
+P = [-1 for _ in range(N)]  
+
+data = list(map(int, sys.stdin.read().split()))
+
+for i in range(len(data) // 2):
+    adj[data[2 * i] - 1].append(data[2 * i + 1] - 1)
+    adj[data[2 * i + 1] - 1].append(data[2 * i] - 1)
+
+DFS(0)
+for j in range(N):
+    P[j] += 1
+print(*P[1:], sep='\n')
