5a84be3c4c5d0a2b9885e96dfeba05986e4d0ac0
Jang CY <wkdcjfdud13@gmail.com>
2024-06-10 12:48:34 +0900

    a

--- barkingdog/solution/그래프/DFS_with_graph.py
+++ barkingdog/solution/그래프/DFS_with_graph.py
@@ -1,2 +1,19 @@
 from collections import deque
 
+Q = deque()
+
+adj = [[1,2],[2],[3],[0,1,2]]
+vis = [False] * 4
+
+def DFS():
+    Q.append(0)
+    vis[0] = True
+    while Q:
+        cur = Q.pop()
+        print(f"Visited node {cur}")
+        for nxt in adj[cur]:
+            if vis[nxt]: continue
+            Q.append(nxt)
+            vis[nxt] = True    
+
+DFS()
