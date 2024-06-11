791b6a301719d7a4109d2ab6d1d890dff6f08a76
Jang CY <wkdcjfdud13@gmail.com>
2024-06-11 20:11:08 +0900

    a

---
+++ barkingdog/solution/그래프+트리+위상정렬/트리입력연습.py
@@ -0,0 +1,26 @@
+import sys
+
+# 백준 https://www.acmicpc.net/problem/1991
+
+N = int(input())
+data = sys.stdin.read().split()
+
+def preDFS(root):
+    
+    if 
+
+
+tree = set()
+prelist = []
+inlist = []
+postlist = []
+
+for i in range(N):
+    tree[data[3 * i]] = (data[3 * i + 1], data[3 * i + 2])
+# '.' 이면 continue인거, 해당값이 
+
+
+    
+
+
+
