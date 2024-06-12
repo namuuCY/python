2dae4bc131643db742c69d37c199e1d0300eb810
Jang CY <wkdcjfdud13@gmail.com>
2024-06-12 16:05:21 +0900

    a

--- barkingdog/solution/그래프+트리+위상정렬/거짓말.py
+++ barkingdog/solution/그래프+트리+위상정렬/거짓말.py
@@ -1,6 +1,14 @@
 import sys
 from collections import deque
 
+def BFS():
+    while Q:
+        cur = Q.popleft()
+        tr[cur] = True
+        for nxt in adj[cur]:
+            if tr[nxt]: continue
+            Q.append(nxt)
+
 Q = deque()
 
 N, M = map(int, input().split())
@@ -14,18 +22,26 @@ if t == "0":
 else:
     tlist = list(map(int, t.split()))
     for i in range(1, tlist[0] + 1):
-        tr[tlist[i]] = True
+        Q.append(tlist[i])
+plist = []
 
 for _ in range(M):
     data = list(map(int, sys.stdin.readline().split()))
     pnum = data[0]
-    for i in range(1, pnum):
-        adj[data[i]].append(data[i + 1])
-        adj[data[i + 1]].append(data[i])
+    plist.append(data[1:])
+    for j in range(1, pnum):
+        adj[data[j]].append(data[j + 1])
+        adj[data[j + 1]].append(data[j])
 
-    
+BFS()
 
+ans = 0
+for k in range(M):
+    for p in plist[k]:
+        if tr[p]:
+            break
+    else:
+        ans += 1
 
-
-
+print(ans)
 
