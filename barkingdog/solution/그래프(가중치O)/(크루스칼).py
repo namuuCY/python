e8aef9139b256f0f75faf553c6361508f4ab32af
Jang CY <wkdcjfdud13@gmail.com>
2024-06-12 19:38:07 +0900

    a

--- barkingdog/solution/그래프(가중치O)/(크루스칼).py
+++ barkingdog/solution/그래프(가중치O)/(크루스칼).py
@@ -24,4 +24,27 @@ class Disjointset:
             if self.rank[root_u] == self.rank[root_v]:
                 self.rank[root_v] += 1
         return True
-    
+
+def kruskal():
+    global ans
+    count = 0
+
+    for u, v, weight in edges:
+        if djset.union(u, v):
+            ans += weight
+            count += 1
+        if count == V - 1:
+            return
+
+V, E = map(int, input().split())
+
+data = list(map(int, sys.stdin.read().split()))
+edges = []
+for i in range(0, 3 * E, 3):
+    edges.append((data[i], data[i + 1], data[i + 2]))
+edges.sort(key = lambda x:x[2])
+ans = 0
+djset = Disjointset(V + 1)
+kruskal()
+
+print(ans)
